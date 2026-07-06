1a87b5942876affa4a66dfd606e57bf6064dbbd6
kevinnjaloung <kevinnjaloung@gmail.com>
2026-07-06 16:58:47 +0200

    build pge for mobile first aproach

--- frontend/app/src/pages/login.tsx
+++ frontend/app/src/pages/login.tsx
@@ -1,20 +1,8 @@
 export function LoginPage()
 {
     return (
-       <div className="w-full h-full bg-gray-950 flex justify-center items-center">
-            <div className="bg-gray-900 w-1/3 h-175 rounded-[15%] border border-purple-400 shadow-[0_30px_100px_rgba(0,0,0,0.6)] shadow-purple-950">
-                <div className="h-1/5 w-full flex justify-center items-center text-white text-6xl">
-                    <h1>Login</h1>
-                </div>
-                <form className="h-1/3 w-full">
-                    <div className=" h-1/2 w-full flex justify-center items-center">
-                        <input placeholder="Joe@example.com" type="email" className="w-8/10 p-3 rounded-3xl bg-gray-800 text-white border border-white focus:outline-none"/>
-                    </div>
-                      <div className=" h-1/2 w-full flex justify-center items-center">
-                        <input placeholder="Password" type="password" className="w-8/10 p-3 rounded-3xl bg-gray-800 text-white border border-white focus:outline-none"/>
-                      </div>
-                     
-                </form>
+       <div className="w-full h-full bg-gray-950 flex justify-center items-center border ">
+            <div className="bg-gray-900 w-full h-full sm:rounded-[15%] sm:border sm:border-purple-400 sm:w-sm sm:h-6/10 md:w-md lg:w-lg xl:w-xl">
             </div>
        </div>
     )
