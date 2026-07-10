34e64c0a01464bacb06c7aea4e2875be3e5f5f54
kevinnjaloung <kevinnjaloung@gmail.com>
2026-07-10 15:29:53 +0200

    making repnoisive design better

--- frontend/app/src/pages/login.tsx
+++ frontend/app/src/pages/login.tsx
@@ -5,7 +5,7 @@ export function LoginPage() {
     return (
         <div className="w-full h-full bg-gray-950 flex justify-center items-center border">
             <div className="bg-gray-900 w-full sm:w-sm h-full sm:rounded-[15%] shadow-lg sm:shadow-purple-200 sm:border
-             sm:border-purple-400  sm:h-6/10 md:w-7/10 md:h-7/10 lg:w-lg xl:w-xl">
+             sm:border-purple-400  sm:h-6/10 md:w-6/10 md:h-7/10 lg:w-lg xl:w-xl">
 
                 <div className="w-full h-2/10 flex justify-center items-center text-white text-5xl">
                     <h1>Login</h1>
