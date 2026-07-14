3e3228b145d696d5b1f20b275938f51f7052ff82
42knjaloun <kevinnjaloung@gmail.com>
2026-07-14 17:09:07 +0200

    limited the size to max 2k resolution

--- frontend/app/src/features/auth/pages/login.tsx
+++ frontend/app/src/features/auth/pages/login.tsx
@@ -3,8 +3,8 @@ import { LoginForm } from "../components/loginForm"
 export function LoginPage() {
 
     return (
-       <div className="h-full w-full flex">
-            <div className="h-full w-full bg-gray-100 md:w-1/2 flex justify-center items-center">
+       <div className="h-full w-full flex max-w-[2560px]">
+            <div className="h-full w-full  bg-gray-100 md:w-1/2 flex justify-center items-center">
                 <LoginForm/>
             </div>
             <div className="h-full w-1/2 md:bg-[url(/img/loginImage.jpg)] hidden md:block md:bg-cover md:bg-center"></div>
