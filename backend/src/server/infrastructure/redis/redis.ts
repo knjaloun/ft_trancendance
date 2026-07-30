080ab1533b4e77480267290072b25b537fae850a
kevinnjaloung <kevinnjaloung@gmail.com>
2026-07-30 19:15:14 +0200

    implemented sessions

--- backend/src/server/infrastructure/redis/redis.ts
+++ backend/src/server/infrastructure/redis/redis.ts
@@ -1,6 +1,7 @@
 import {Redis} from 'ioredis';
 import dotenv from "dotenv";
 import dotenvExpand from "dotenv-expand";
+import { createClient } from "redis";
 
 
 if (!process.env.REDIS_HOST)
@@ -13,3 +14,12 @@ export const redis_connection = new Redis({maxRetriesPerRequest: null,
                                             host: process.env.REDIS_HOST,
                                             port: Number(process.env.REDIS_PORT)
 })
+
+export const session_redis_connection = createClient({
+  socket: {
+    host: process.env.REDIS_HOST,
+    port: Number(process.env.REDIS_PORT),
+  },
+});
+
+await session_redis_connection.connect();
