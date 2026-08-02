e258572d92125242793037060ac07b5e9f08b692
42knjaloun <kevinnjaloung@gmail.com>
2026-08-02 15:16:14 +0200

    teh 2fa ubput text are now in sync

--- frontend/app/src/features/2fa/pages/2faVweifyPage.tsx
+++ frontend/app/src/features/2fa/pages/2faVweifyPage.tsx
@@ -1,7 +1,43 @@
+import { useState, useRef } from "react"
+import { type ChangeEvent } from "react";
+
+
+const pattern = /^\d*$/;
 
 export function TwoFaVerifyPage() {
 
-return(
+    const [code, setcode] = useState(Array(6).fill(""));
+
+    const inputRef = useRef<(HTMLInputElement | null)[]>([]);
+
+
+    const handleInputChange = (e: ChangeEvent<HTMLInputElement>, index: number) => {
+        console.log(`modeifing index : ${index}`)
+        let updated_code = e.target.value;
+        if (!pattern.test(updated_code))
+            updated_code = ""
+        const new_code = [...code];
+
+        new_code[index] = updated_code
+
+        setcode(new_code)
+        if (index < 5 && updated_code)
+            inputRef.current[index + 1]?.focus();
+    }
+
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>, index: number, value: string) => {
+
+        if (
+            e.key === "Backspace" &&
+            e.currentTarget.value === "" &&
+            index > 0
+        ) {
+            inputRef.current[index - 1]?.focus();
+        }
+    }
+
+    return (
+
         <div className="w-full h-full flex items-center justify-center">
             <div className="h-8/10 w-8/10 bg-gray-100 max-w-150 max-h-200">
                 <div className="h-1/10 w-full flex items-center justify-center">
@@ -11,11 +47,21 @@ return(
                     <p className="text-gray-400">we have sent you the code to your email inbox</p>
                 </div>
                 <div className="h-1/10 w-full flex justify-center items-center gap-2">
-                    <div className="flex gap-2">
-                       
-                    </div>
+                    {code.map((item: string, index: number) => (
+                        <input
+                            autoFocus={index === 0}
+                            key={index}
+                            ref={(el: HTMLInputElement | null) => { inputRef.current[index] = el; }}
+                            className="h-7/10 w-1/8 bg-white caret-transparent text-2xl border border-gray-400"
+                            maxLength={1}
+                            value={code[index]}
+                            onChange={(e) => { handleInputChange(e, index) }}
+                            onKeyDown={(e) => handleKeyDown(e, index, item)}
+                        />
+                    )
+                    )}
+
                 </div>
-                <div></div>
             </div>
         </div>
 
